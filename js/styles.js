d43e2b0dcd98b6f91f046865c7289a06882bf515
Timna Schulze <Timna.lansel@gmail.com>
2023-01-29 17:02:48 -0800

    add JavaScript and section called form to practice arrays

---
+++ js/styles.js
@@ -0,0 +1,9 @@
+window.onload = function(){
+  let form = document.querySelector("form");
+  form.addEventListener("submit",function(event) {
+
+    const siblings = [Anna, Taylor]
+    const parents = [Harry, Lisa]
+    array.prototype.concat (siblings, parents)
+  });
+};
